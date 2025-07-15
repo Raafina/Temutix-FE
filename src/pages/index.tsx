0a1ca8a679e08d9a1133443501d9f8ea031f4d2e
Raafina <raafinuradzani16@gmail.com>
2025-07-15 21:54:15 +0700

    update: index page temp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,43 @@
-'use client';
+"use client";
 
 import { Inter } from "next/font/google";
 import PageHead from "@/components/commons/PageHead";
 import { Button } from "@heroui/react";
+import Image from "next/image";
+import router from "next/router";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`${inter.className} flex min-h-screen flex-col items-center justify-center px-96`}
     >
       <PageHead />
-      <Button color="primary">Button</Button>
+      <div className="space-y-6 rounded-lg border p-10">
+        <Image
+          src="/images/general/logo.svg"
+          alt="Logo"
+          width={200}
+          height={200}
+        />
+        <p>
+          Temutix is a comprehensive online platform designed to simplify your
+          event experience. Whether you're looking for concerts, workshops,
+          sports matches, or cultural festivals, Temutix provides a seamless
+          solution for discovering and purchasing tickets for a wide variety of
+          events. With an intuitive interface and secure transaction processes,
+          Temutix makes finding and securing your spot at any event quick and
+          hassle-free.
+        </p>
+        <Button
+          color="secondary"
+          onPress={() => {
+            router.push("/admin/dashboard");
+          }}
+        >
+          Get Started
+        </Button>
+      </div>
     </main>
   );
 }
