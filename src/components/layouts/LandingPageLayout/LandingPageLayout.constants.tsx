674f688d49c2d0a966e42d6b5eb4a5731d2d9097
Raafina <raafinuradzani16@gmail.com>
2025-07-26 21:07:07 +0700

    feat: add footer UI

--- src/components/layouts/LandingPageLayout/LandingPageLayout.constants.tsx
+++ src/components/layouts/LandingPageLayout/LandingPageLayout.constants.tsx
@@ -1,25 +1,47 @@
+import {
+  FaFacebook,
+  FaInstagram,
+  FaTiktok,
+  FaTwitter,
+  FaYoutube,
+} from "react-icons/fa6";
+
 const NAV_ITEMS = [
-  {
-    label: "Home",
-    href: "/",
-  },
-  {
-    label: "Explore",
-    href: "/event",
-  },
+  { label: "Home", href: "/" },
+  { label: "Explore", href: "/event" },
 ];
 
 const BUTTON_ITEMS = [
+  { label: "Register", href: "/auth/register", variant: "bordered" },
+  { label: "Login", href: "/auth/login", variant: "solid" },
+];
+
+const SOCIAL_ITEMS = [
   {
-    label: "Register",
-    href: "/auth/login",
-    variant: "bordered",
+    label: "Facebook",
+    href: "/",
+    icon: <FaFacebook />,
   },
   {
-    label: "Login",
-    href: "/auth/register",
-    variant: "solid",
+    label: "Instagram",
+    href: "/",
+    icon: <FaInstagram />,
+  },
+  {
+    label: "TikTok",
+    href: "/",
+    icon: <FaTiktok />,
+  },
+  {
+    label: "Twitter",
+    href: "/",
+    icon: <FaTwitter />,
+  },
+  {
+    label: "YouTube",
+    href: "/",
+    icon: <FaYoutube />,
   },
 ];
 
-export { NAV_ITEMS, BUTTON_ITEMS };
+export { NAV_ITEMS, BUTTON_ITEMS, SOCIAL_ITEMS };
